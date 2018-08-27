aaf4304d09be480500496eb6c1be0133c0ab87e2
Seiya <seiya1223@hotmail.com>
2018-08-27 14:12:31 +0800

    Init

---
+++ SPandGraph/GraphTest.NetcoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphTest.NetcoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
